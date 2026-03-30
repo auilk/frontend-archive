9e446d1c2af595efd9aad3a4b73a5e685eb5dce8
auilk <abdelghafour.lbaz01@gmail.com>
2026-03-30 22:11:34 +0100

    chore(qr-code-component/react): initialize Vite project and add Tailwind CSS

---
+++ challenges/qr-code-component/react/src/App.jsx
@@ -0,0 +1,10 @@
+function App()
+{
+  return(
+    <main className="w-dvw h-dvh flex justify-center items-center bg-[#141414]">
+      <h1 className="text-9xl text-white font-bold">Hello, World!</h1>
+    </main>
+  );
+}
+
+export default App;
